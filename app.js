b36f51c4ed4ad4e2a0b6bc19294c2826a04d0669
James Kelly <jk_jameskelly@outlook.com>
2017-03-24 13:48:33 -0400

    mongo with initial data

--- app.js
+++ app.js
@@ -1,8 +1,48 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+var db = mongoose.connect('mongodb://localhost/bookAPI');
+
+var Book = require('./models/bookModel');
 
 var app = express();
 
 var port = process.env.PORT || 3000;
+
+var bookRouter = express.Router();
+
+bookRouter.route('/Books')
+  .get(function(req, res) {
+    var query = {};
+
+    if (req.query.genre) {
+      query.genre = req.query.genre;
+    }
+    Book.find(query, function(err, books) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(books);
+      }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+  .get(function(req, res) {
+
+    Book.findById(req.params.bookId, function(err, book) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(book);
+      }
+    });
+  });
+
+bookRouter.route('/Books/:bookId')
+app.use('/api', bookRouter);
+
+
 
 app.get('/', function(req, res) {
   res.send('welcome to my api!!');
